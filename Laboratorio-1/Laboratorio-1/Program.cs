83534b993416d565ab03158c220c3af40e8e306b
Sebastián Altmann <saaltman@miuandes.cl>
2020-03-18 19:31:35 -0300

    First commit

---
+++ Laboratorio-1/Laboratorio-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Laboratorio1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
